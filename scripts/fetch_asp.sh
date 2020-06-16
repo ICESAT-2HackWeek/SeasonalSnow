6a30423bfeddf2299979accd819afd409f79bb9c
ShashankBice <sbaglapl@uw.edu>
2020-06-16 17:31:31 +0000

    quick asp download script

---
+++ scripts/fetch_asp.sh
@@ -0,0 +1,7 @@
+cd ~/
+mkdir src
+cd src/
+wget https://github.com/NeoGeographyToolkit/StereoPipeline/releases/download/v2.6.2/StereoPipeline-2.6.2-2019-06-17-x86_64-Linux.tar.bz2
+tar -xvjf StereoPipeline-2.6.2-2019-06-17-x86_64-Linux.tar.bz2
+rm -rf StereoPipeline-2.6.2-2019-06-17-x86_64-Linux.tar.bz2
+mv StereoPipeline-2.6.2-2019-06-17-x86_64-Linux StereoPipeline
